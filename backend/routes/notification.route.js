ab93015ea67baab0473121c7db2b4e4036418a04
saibhagat2004 <saibhagat2004@gmail.com>
2024-08-02 10:27:37 +0530

    UI Design completed

--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/protectRoute.js"
-import {getNotifications,deleteNotifications,deleteOneNotification} from "../controllers/notification.controller.js"
+import {getNotifications,deleteNotifications} from "../controllers/notification.controller.js"
 const router=express.Router()
 
 router.get("/",protectRoute,getNotifications);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
+
+//by wraping App inside BrowserRouter  we can use any component come from react-router dom  inside app.js
