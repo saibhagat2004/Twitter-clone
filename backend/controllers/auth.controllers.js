b48a7ba188170c7faa2eb3e1f22fa6402aa53f04
saibhagat2004 <saibhagat2004@gmail.com>
2024-08-13 22:31:13 +0530

    include vercel build

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,62 +1,7 @@
 import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs'; 
-// export const signup=async(req,res)=>{
-//        try{
-//             const{fullName, username, email, password}=req.body;
-//             const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  //this regex pattern ensures that the email has the general format 
-//             if (!emailRegex.test(email)) {                    //This line checks if the provided email string does not match the emailRegex pattern. The test method returns true if the email matches the regex pattern and false otherwise.
-//                 return res.status(400).json({ error: "Invalid email format" });
-//             }
-//             const existingUser= await User.findOne({username});
-//             if(existingUser){
-//                 return res.status(400).json({error:"Username already taken"});
-//             }
-//             const existingaEmail= await User.findOne({email});
-//             if(existingaEmail){
-//                 return res.status(400).json({error:"Email already taken"});
-//             }
 
-//             if(password.length < 6){
-//                 return res.status(400).json({error:"Password must be least 6 character long."})
-//             }
-//             //hash password
-//             const salt= await bcrypt.genSalt(10);
-//             const hashPassword= await bcrypt.hash(password, salt)
-
-//             const newUser= new User({
-//                 fullName,        //generally it is written as fullName:fullName but since syntex name is same it direlyt put or store value .
-//                 username,
-//                 email,
-//                 password:hashPassword
-//             })
-
-//             if(newUser){
-//                 generateTokenAndSetCookie(newUser._id,res)
-//                 await newUser.save();
-
-//                 res.status(201).json({
-//                     _id: newUser._id,
-//                     fullName: newUser.fullName,
-//                     username: newUser.username,
-//                     email: newUser.email,
-//                     followers: newUser.followers,
-//                     following: newUser.following,
-//                     profileImg: newUser.profileImg,
-//                     coverImg: newUser.coverImg,
-//                 })
-            
-//             }else{
-//                     res.status(400).json({error:"Invalid user data"});
-//                 }
-            
-//         }  
-//        catch(error){
-//             console.log("Error in signup controller", error.message);
-//             res.status(500).json({error:"Internal Server Error"});
-//        }
-    
-// }
 export const signup = async (req, res) => {
 	try {
 		const { fullName, username, email, password } = req.body;
--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -1,6 +1,6 @@
  import jwt from 'jsonwebtoken';
 
- export const    generateTokenAndSetCookie=(userId,res)=>{
+ export const  generateTokenAndSetCookie=(userId,res)=>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{
         expiresIn:'15d'
     })
