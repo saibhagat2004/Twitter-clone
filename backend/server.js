db24cec3230cd09ba61fa6c7e055858440bbbae8
saibhagat2004 <saibhagat2004@gmail.com>
2024-08-23 12:25:42 +0530

    Revert "prepare app for deployment"
    This reverts commit 381dd36bdd2b84c782751afd8eb23725044de4be.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // 
 import path from "path";
 import express from "express";
@@ -5,6 +6,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
+=======
+import express from "express";
+>>>>>>> parent of 381dd36 (prepare app for deployment)
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -21,13 +25,18 @@ cloudinary.config({
 });
 
 const app = express();
+<<<<<<< HEAD
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
+=======
+const PORT=process.env.PORT || 5000
+>>>>>>> parent of 381dd36 (prepare app for deployment)
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
+<<<<<<< HEAD
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -42,6 +51,12 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
+=======
+ app.use("/api/auth",authRoutes);
+ app.use("/api/users",userRoutes);
+ app.use("/api/posts",postRoutes);
+ app.use("/api/notifications",notificationRoutes);
+>>>>>>> parent of 381dd36 (prepare app for deployment)
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
