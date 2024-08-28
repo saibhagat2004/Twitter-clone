a2dc7fb384e6f8dcbbd5fc93a744a02b2028ee27
saibhagat2004 <saibhagat2004@gmail.com>
2024-08-28 19:12:48 +0530

    sign up auto direct to home page fixed.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools/>
     </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
